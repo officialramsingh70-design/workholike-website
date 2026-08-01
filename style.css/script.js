febdd4ccecd9623ec1d6513140db5336c9e13ec7
Ram Singh <ram@workholikesolutions>
2026-08-02 00:04:13 +0530

    first website

---
+++ style.css/script.js
@@ -0,0 +1,18 @@
+body{
+    margin:0;
+    font-family:Arial, Helvetica, sans-serif;
+    background:#0f172a;
+    color:white;
+    text-align:center;
+    padding-top:120px;
+}
+
+h1{
+    font-size:55px;
+    margin-bottom:20px;
+}
+
+p{
+    font-size:24px;
+    color:#cbd5e1;
+}
