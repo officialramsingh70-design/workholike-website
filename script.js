d39ad79e24ea86aab23265c5e0a003d2d331f1d3
Ram Singh <ram@workholikesolutions>
2026-08-02 12:01:12 +0530

    Update website

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Website Loaded");
+// Sticky Navbar Shadow
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        nav.style.background = "rgba(0,0,0,0.92)";
+        nav.style.boxShadow = "0 5px 25px rgba(0,210,106,.15)";
+    } else {
+        nav.style.background = "rgba(0,0,0,.75)";
+        nav.style.boxShadow = "none";
+    }
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if(target){
+            target.scrollIntoView({
+                behavior:"smooth"
+            });
+        }
+    });
+});
